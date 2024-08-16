32f466df6ba529bf4751cea7ff571f791f9020e6
victorhugo-neves <victorhugo06neves@gmail.com>
2024-08-16 01:30:22 -0300

    segundo repositorio

--- curso.js
+++ curso.js
@@ -1,1 +1,14 @@
-console.log ("ola mundão")
+let nome = "Seunome";
+let nivel = 7500;
+
+     if (nivel <=1000 ) { console.log("voce esta no nivel ferro")
+}    else if (nivel >1.000 <=2.000) { console.log("voce esta no nivel bronze")
+}    else if (nivel >2.000 && nivel <=5.000) { console.log("voce esta no nivel prata")
+}    else if (nivel >5.000 && nivel <=7.000) { console.log("voce esta no nivel ouro")
+}    else if (nivel >7.000 && nivel <=8.000) { console.log("voce esta no nivel platina")
+}    else if (nivel >8.000 && nivel <=9.000) { console.log("voce esta no nivel ascendente")
+}    else if (nivel >9.000 && nivel <=10.000) { console.log("voce esta no nivel imortal")}
+     else { console.log("voce esta no nivel radiante")
+} 
+
+console.log ( "o heroi de nome " + nome + "esta no nivel de" +nivel )
